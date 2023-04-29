a957482a1bdeb8fa0fc3177fe694f51af5af1f14
Lawrence Simon <lawsimon3105@gmail.com>
2023-04-29 09:36:15 +0200

    Added code that prevents two operator signs from being added consecutively

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let caret_position; // holds the position of the carets recent position
+const ope_rgx = /[+*-\/]/; // operator regex
 
 function captureButton(btn) {
 	console.log(btn.value);
@@ -9,7 +10,6 @@ function captureButton(btn) {
 		if (btn.type == "click") {
 			displayCalculation(btn.target.innerHTML); 
 		}
-		//displayPendingResult(); 
 	}
 }
 
@@ -33,14 +33,21 @@ function displayCalculation(captured_value) {
 		}
 	}
 	else {
-		if (caret_position === 0) {
+		if (select_cd.value.length === 0 || caret_position === select_cd.value.length-1) {
 			select_cd.value = select_cd.value + captured_value;
+			caret_position = select_cd.value.length-1;
 		}
 		else {
-			temp_arr.splice(caret_position, 0, captured_value);
-			select_cd.value = temp_arr.join("");
-			select_cd.selectionStart = caret_position + 1;
-			caret_position = select_cd.selectionStart;
+			if (ope_rgx.test(captured_value) && (ope_rgx.test(temp_arr[caret_position]) || ope_rgx.test(temp_arr[caret_position-1]))) {
+				console.log(captured_value);
+				return null;
+			}
+			else {
+				temp_arr.splice(caret_position, 0, captured_value);
+				select_cd.value = temp_arr.join("");
+				select_cd.selectionStart = caret_position + 1;
+				caret_position = select_cd.selectionStart;
+			}
 		}
 	}
 }
